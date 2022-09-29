9caecde77fc6f4fc54bedf73bc5911f0e9234901
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-09-29 07:30:04 +0530

    Responsive widget created

---
+++ lib/responsive/responsive.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+/// File used to create Responsive Application in Flutter
+/// ### Making Desktop disable, as this is mobile Browser ###
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget? desktop;
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    required this.tablet,
+    this.desktop,
+  }) : super(key: key);
+
+  // screen sizes
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1000 &&
+      MediaQuery.of(context).size.width >= 600;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1000;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= 1000) {
+          return desktop ?? const SizedBox();
+        } else if (constraints.maxWidth >= 600) {
+          return tablet;
+        } else {
+          return mobile;
+        }
+      },
+    );
+  }
+}
