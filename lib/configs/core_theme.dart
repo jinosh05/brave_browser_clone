c1f386f2dd9783b203a058138801f591e789839f
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-09-29 07:44:58 +0530

    Config files created

---
+++ lib/configs/core_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// Need to Use this Font Family
+// const fontFamily = "SF UI Display";
+const fontFamily = 'Poppins';
+
+final themeLight = ThemeData(
+  primaryColorLight: const Color(0xffC0392B),
+  brightness: Brightness.light,
+  primaryColor: const Color(0xffC0392B),
+  highlightColor: Colors.black,
+  canvasColor: Colors.white,
+  fontFamily: fontFamily,
+  backgroundColor: Colors.white,
+  splashColor: Colors.transparent,
+  scaffoldBackgroundColor: Colors.white,
+  colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red).copyWith(
+    secondary: Colors.black,
+    brightness: Brightness.light,
+  ),
+);
+
+final themeDark = ThemeData(
+  brightness: Brightness.dark,
+  primaryColorDark: const Color(0xffC0392B),
+  primaryColor: const Color(0xffC0392B),
+  highlightColor: const Color(0xffC0392B),
+  canvasColor: Colors.white,
+  fontFamily: fontFamily,
+  splashColor: Colors.transparent,
+  backgroundColor: Colors.grey[800],
+  scaffoldBackgroundColor: Colors.black,
+  colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red).copyWith(
+    secondary: const Color(0xffC0392B),
+    brightness: Brightness.dark,
+  ),
+);
