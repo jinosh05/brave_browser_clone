eac2d5f84ac37e310b7c36d031a726a03007ca1c
jinosh05 <jinoshprabhuraj5@gmail.com>
2022-10-02 22:54:42 +0530

    AppColors class created

---
+++ lib/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  static const List<Color> linGrad = [Color(0xff83288A), Color(0xffA12476)];
+}
--- lib/screens/dummy.dart
+++ lib/screens/dummy.dart
@@ -10,6 +10,8 @@ class Dummy extends StatefulWidget {
 class _DummyState extends State<Dummy> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
